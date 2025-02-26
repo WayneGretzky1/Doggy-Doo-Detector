e882538c1593351dc687d8c25913a558166f9a24
WayneGretzky1 <brockbye@outlook.com>
2025-02-26 09:53:04 -0600

    "main onto brock"

--- UI_WebApp/PETRA/PETRA/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/PETRA.AssemblyInfo.cs
+++ UI_WebApp/PETRA/PETRA/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/PETRA.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PETRA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ec8dad568605a638ae98db8937cc1f830655956c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+512d49f23f2afca93b948909f0f1669be6ec5b65")]
 [assembly: System.Reflection.AssemblyProductAttribute("PETRA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PETRA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
