0193e9a623e99643a15cd49204110d5072cc3f11
WayneGretzky1 <brockbye@outlook.com>
2025-02-26 10:12:02 -0600

    "Merged changes"

--- UI_WebApp/PETRA/PETRA/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/PETRA.AssemblyInfo.cs
+++ UI_WebApp/PETRA/PETRA/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/PETRA.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PETRA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+512d49f23f2afca93b948909f0f1669be6ec5b65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d99f727e0bd4fd9a8db6fc6e9eb2f6704c8033f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PETRA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PETRA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
