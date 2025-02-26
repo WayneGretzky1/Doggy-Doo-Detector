d99f727e0bd4fd9a8db6fc6e9eb2f6704c8033f5
WayneGretzky1 <brockbye@outlook.com>
2025-02-26 09:43:17 -0600

    "viewmodel"

--- UI_WebApp/PETRA/PETRA/MainPage.xaml.cs
+++ UI_WebApp/PETRA/PETRA/MainPage.xaml.cs
@@ -1,26 +1,18 @@
-﻿namespace PETRA
+﻿using PETRA.ViewModel;
+
+namespace PETRA
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
 
-        public MainPage()
+        public MainPage(MainViewModel vm)
         {
             InitializeComponent();
+            BindingContext = vm;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            // test brock
-            count+=10;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
     }
 
 }
--- UI_WebApp/PETRA/PETRA/MauiProgram.cs
+++ UI_WebApp/PETRA/PETRA/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using PETRA.ViewModel;
 
 namespace PETRA
 {
@@ -14,6 +15,9 @@ namespace PETRA
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+
+            builder.Services.AddSingleton<MainPage>();
+            builder.Services.AddSingleton<MainViewModel>();
 
 #if DEBUG
     		builder.Logging.AddDebug();
